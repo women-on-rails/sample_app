62badc1809122aaffb69930afc93107d71d4904b
Juliette Audema <juliette.audema@gmail.com>
2020-01-26 20:14:27 +0100

    Adds first relations and seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+20.times do
+  User.create(first_name: Faker::Dessert.variety, last_name: Faker::Dessert.topping, birthdate: Time.at(0 + rand * (Time.now.to_f - 0.to_f)).to_date)
+end
+
+50.times do
+  Post.create(user: User.all.sample, title: Faker::Dessert.flavor, content: Faker::GreekPhilosophers.quote)
+  Comment.create(user: User.all.sample, post: Post.all.sample)
+end
